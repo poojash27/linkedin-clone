3b6a8c0ddf065c2b0ebc33643aed5f0188aaea0c
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 03:26:48 +0530

    Building the Post Section

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Feed.css';
 import InputOption from './InputOption';
+import Post from './Post';
 import CreateRoundedIcon from '@mui/icons-material/CreateRounded';
 import ImageRoundedIcon from '@mui/icons-material/ImageRounded';
 import SubscriptionsRoundedIcon from '@mui/icons-material/SubscriptionsRounded';
@@ -8,26 +9,41 @@ import EventNoteRoundedIcon from '@mui/icons-material/EventNoteRounded';
 import NotesRoundedIcon from '@mui/icons-material/NotesRounded';
 
 function Feed() {
-  return (
-    <div className="feed">
-        <div className="feed__inputContainer">
-            <div className="feed__input">
-                <CreateRoundedIcon />
-                <form action="">
-                    <input type="text" placeholder="Start a post" />
-                    <button type="submit">Send</button>
-                </form>
+    const [posts, setPosts] = useState([]);
+    const sendPost = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateRoundedIcon />
+                    <form action="">
+                        <input type="text" placeholder="Start a post" />
+                        <button onClick={sendPost} type="submit">Send</button>
+                    </form>
+                </div>
+
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageRoundedIcon} title="Photo" iconColor="#70B5F9" />
+                    <InputOption Icon={SubscriptionsRoundedIcon} title="Video" iconColor="#7FC15E" />
+                    <InputOption Icon={EventNoteRoundedIcon} title="Event" iconColor="#E7A33E" />
+                    <InputOption Icon={NotesRoundedIcon} title="Write an article" iconColor="#FC9295" />
+                </div>
             </div>
 
-            <div className="feed__inputOptions">
-                <InputOption Icon={ImageRoundedIcon} title="Photo" iconColor="#70B5F9" />
-                <InputOption Icon={SubscriptionsRoundedIcon} title="Video" iconColor="#7FC15E" />
-                <InputOption Icon={EventNoteRoundedIcon} title="Event" iconColor="#E7A33E" />
-                <InputOption Icon={NotesRoundedIcon} title="Write an article" iconColor="#FC9295" />
-            </div>
+            {/* Posts */}
+            {posts.map((post) => {
+                <Post />
+            })}
+            <Post 
+                name="David Brown" 
+                description="Intern at Microsoft" 
+                message="WOW this worked!" 
+            />
         </div>
-    </div>
-  );
+    );
 }
 
 export default Feed;
---
+++ src/components/Post.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Post.css';
+import InputOption from './InputOption';
+import { Avatar } from '@mui/material';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ShareRoundedIcon from '@mui/icons-material/ShareRounded';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
+
+function Post({ name, description, message, photoUrl }) {
+    return <div className="post">
+        <div className="post__header">
+            <Avatar src={photoUrl} />
+            <div className="post__info">
+                <h2>{name}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+
+        <div className="post__body">
+            <p>{message}</p>
+        </div>
+
+        <div className="post__buttons">
+            <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" iconColor="#666" />
+            <InputOption Icon={ChatOutlinedIcon} title="Comment" iconColor="#666" />
+            <InputOption Icon={ShareRoundedIcon} title="Share" iconColor="#666" />
+            <InputOption Icon={SendRoundedIcon} title="Send" iconColor="#666" />
+        </div>
+    </div>;
+}
+
+export default Post;
