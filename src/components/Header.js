6455647c10448b1b767f067a1c0e9dd28e615a09
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 00:58:40 +0530

    Building the Header Options

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderOption from './HeaderOption';
 import logo from './logo.png';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import SupervisorAccountRoundedIcon from '@mui/icons-material/SupervisorAccountRounded';
+import BusinessCenterRoundedIcon from '@mui/icons-material/BusinessCenterRounded';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
 
 function Header() {
   return (
@@ -15,7 +21,14 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+          <HeaderOption Icon={HomeRoundedIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountRoundedIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenterRoundedIcon} title="Jobs" />
+          <HeaderOption Icon={ChatRoundedIcon} title="Messaging" />
+          <HeaderOption Icon={NotificationsRoundedIcon} title="Notifications" />
+          <HeaderOption avatar="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" title="Me" />
+      </div>
     </div>
   );
 }
