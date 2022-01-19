55377f483e9b2c9645101adb564608eb4d2ef230
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 01:57:45 +0530

    Building the Sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar } from '@mui/material';
+
+function Sidebar() {
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    );
+
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img className="sidebar__bg" src="https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=434&q=80" alt="" />
+                <Avatar className="sidebar__avatar" src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+
+                <h2>David Brown</h2>
+                <h4>david.brown@gmail.com</h4>
+            </div>
+
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">2,543</p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">2,448</p>
+                </div>
+            </div>
+
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+                {recentItem("reactjs")}
+                {recentItem("programming")}
+                {recentItem("softwareengineering")}
+                {recentItem("design")}
+                {recentItem("development")}
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
