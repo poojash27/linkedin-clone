d36e35a93528a0fc4840817ae02dab644f361771
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 02:36:31 +0530

    Building the Input Option

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Feed.css';
+import InputOption from './InputOption';
 import CreateRoundedIcon from '@mui/icons-material/CreateRounded';
+import ImageRoundedIcon from '@mui/icons-material/ImageRounded';
+import SubscriptionsRoundedIcon from '@mui/icons-material/SubscriptionsRounded';
+import EventNoteRoundedIcon from '@mui/icons-material/EventNoteRounded';
+import NotesRoundedIcon from '@mui/icons-material/NotesRounded';
 
 function Feed() {
   return (
@@ -12,6 +17,13 @@ function Feed() {
                     <input type="text" placeholder="Start a post" />
                     <button type="submit">Send</button>
                 </form>
+            </div>
+
+            <div className="feed__inputOptions">
+                <InputOption Icon={ImageRoundedIcon} title="Photo" iconColor="#70B5F9" />
+                <InputOption Icon={SubscriptionsRoundedIcon} title="Video" iconColor="#7FC15E" />
+                <InputOption Icon={EventNoteRoundedIcon} title="Event" iconColor="#E7A33E" />
+                <InputOption Icon={NotesRoundedIcon} title="Write an article" iconColor="#FC9295" />
             </div>
         </div>
     </div>
