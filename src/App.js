3d16967f421d97cbfcc534d9ae3b0dc3ffa14153
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 16:18:19 +0530

    Building the Login Page and Functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 import './App.css';
 import Header from './components/Header';
+import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoURL: userAuth.photoURL,
+          })
+        );
+      } else {
+        // user logged out
+        dispatch(logout());
+      }
+    })
+  }, []);
 
   return (
     <div className="app">
