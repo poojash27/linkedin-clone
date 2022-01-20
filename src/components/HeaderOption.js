b4b9fe7d5b035403540d4361d8ca486e97309806
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 17:23:12 +0530

    Bug fix HeaderOption was throwing an error when rendered on Login Page because user was null

--- src/components/HeaderOption.js
+++ src/components/HeaderOption.js
@@ -10,10 +10,17 @@ function HeaderOption({ avatar, Icon, title, onClick }) {
   return <div onClick={onClick} className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
 
-      { avatar && 
-          <Avatar src={user.photoURL} className="headerOption__icon" >
+      {/* { avatar && user && 
+          <Avatar src={user.photoURL} className="headerOption__icon">
             {user.email[0]}
           </Avatar>
+      } */}
+
+      { avatar && 
+        (!user 
+          ? <Avatar className="headerOption__icon" /> // Login Page
+          : <Avatar src={user.photoURL} className="headerOption__icon">{user.email[0]}</Avatar> // Feed Page
+        )
       }
       
       <h3 className="headerOption__title">{title}</h3>
