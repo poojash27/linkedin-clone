b15894a4859ee5e0c48d3bf3ad88ffc769c0bdba
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 13:58:38 +0530

    Integrating Redux Using redux to create userSlice that helps login and logout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {/* If there is no user, render out Login Page. Otherwise render the app__body. */}
+      {!user 
+        ? <Login />
+        : (<div className="app__body">
+              <Sidebar />
+              <Feed />
+              {/* Widgets */}
+           </div>)
+      }
     </div>
   );
 }
