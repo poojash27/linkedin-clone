768125fae05d891b649be45944b1a94699502af0
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-20 18:12:24 +0530

    Building the Widgets Section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
+import Widgets from './components/Widgets';
 
 function App() {
   const user = useSelector(selectUser);
@@ -41,7 +42,7 @@ function App() {
         : (<div className="app__body">
               <Sidebar />
               <Feed />
-              {/* Widgets */}
+              <Widgets />
            </div>)
       }
     </div>
